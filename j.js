9ba39d90b283442f99ae316de3ebe740c79979c6
prabin <bhandariprabin402@gmail.com>
2023-12-22 19:38:42 +0545

    add j

---
+++ j.js
@@ -0,0 +1,10 @@
+let a =Number(prompt("enter the number")) ;
+function reverse the nmber(a)
+{
+ a=a+"";
+ return navigator.split("").reverse().join("");
+}
+
+console.log(number(reverse_a_number(a)))
+
+    
